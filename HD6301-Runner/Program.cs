d0c51a707f622e6cbf58582eb80cce625b16d792
_nebula <41904486+misternebula@users.noreply.github.com>
2025-05-11 23:53:42 +0100

    Initial commit

---
+++ HD6301-Runner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HD6301_Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
